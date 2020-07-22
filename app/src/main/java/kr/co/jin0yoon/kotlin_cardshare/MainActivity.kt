89e4a3fd6c9658cf9ba88769823eed6ce3e2480a
Yoon Jinyeong <wwwz4805@naver.com>
2020-07-22 18:25:17 +0900

    Update MainActivity.kt
    Update ExternalStoragePermission

--- app/src/main/java/kr/co/jin0yoon/kotlin_cardshare/MainActivity.kt
+++ app/src/main/java/kr/co/jin0yoon/kotlin_cardshare/MainActivity.kt
@@ -10,9 +10,13 @@ ShareImage(File):Unit                  -> File 공유하기
 DeleteTextAndImaeFile():Unit           -> EditText 및 File 지우기
  */
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,10 +26,30 @@ class MainActivity : AppCompatActivity() {
         //타이틀을 숨긴다.
         setHideTitle()
         setContentView(R.layout.activity_main)
+
+        //퍼미션을 사용자가 눈으로 손으로 확인하게 하는 함수
+        //구글 정책. 구글이 정책으로 만들면 개발자는 무조건 해야 함.
+        grantExternalStoragePermission()
     }
 
     private fun setHideTitle(){
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         getSupportActionBar()!!.hide()
+    }
+
+    //0. 마시멜로우 버전 이후부터는 정책상 반드시 해야 하는 과정
+    private fun grantExternalStoragePermission():Boolean {
+        //마시멜로우 버전 이상이면
+        return if (Build.VERSION.SDK_INT >= 23){
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                true
+            }else{
+                //퍼미션이 허락되어있지 않다면 허락을 요청하는 창을 띄운다.
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+                false
+            }
+        }else{
+            true
+        }
     }
 }
